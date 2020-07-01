7486e505ef293894d484bdb70f5de00f05adab4d
asmintamvan <asmin987asmin@gmail.com>
2020-07-01 18:38:09 +0800

    initial to heroku

---
+++ run.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request
+from os import environ
+import requests
+app = Flask(__name__)
+url = 'https://api.telegram.org/bot' + environ['TOKEN']
+
+
+
+
+def update(update):
+    if 'new_chat_member' in str(update):
+        nama_grup = update['message']['chat']['title']
+        grup_id   = update['message']['chat']['id']
+        mem_baru  = update['message']['new_chat_member']['first_name']
+        teks      = f'Hai {mem_baru} !\nSelamat datang di Grup {nama_grup}'
+        kirim_pesan(grup_id,teks)
+    else:
+        id = update['message']['chat']['id']
+        kirim_pesan(id,'halo, saya bot pertama')
+def kirim_pesan(id,teks):
+    data = {
+            'chat_id':id,
+            'text':teks
+            }
+    requests.get(url+'sendMessage',params=data)
+
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        data_update = request.get_json()
+        update(data_update)
+        return "oke"
+    else:
+        return 'HALAMAN BOT PERTAMA SAYAHALAMAN INI BISA DI GANTI DENGAN KODE HTML'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=int(environ.get('PORT','5000')),debug=True)
+
