323febff93622ee7e426b4626c526c6deb707f20
asmintamvan <asmin987asmin@gmail.com>
2020-07-02 12:58:42 +0800

    fix add answer

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 from os import environ
 import requests
 app = Flask(__name__)
-url = 'https://api.telegram.org/bot' + environ["TOKEN"] + "/"
+url = 'https://api.telegram.org/bot' + environ["TOKEN"]
 
 
 
@@ -16,19 +16,19 @@ def update(update):
         grup_id   = update['message']['chat']['id']
         mem_baru  = update['message']['new_chat_member']['first_name']
         teks      = f'Hai {mem_baru} !\nSelamat datang di Grup {nama_grup}'
-        kirim_pesan(grup_id,teks)
+        kirim_pesan(grup_id, teks)
     else:
         id = update['message']['chat']['id']
-        kirim_pesan(id,'halo, smbf-bot')
-def kirim_pesan(id,teks):
+        kirim_pesan(id, update['message']['chat']['text'])
+def kirim_pesan(id, teks):
     data = {
             'chat_id':id,
             'text':teks
             }
-    requests.get(url+'sendMessage',params=data)
+    requests.get(url  + '/sendMessage', params=data)
 
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/',methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         data_update = request.get_json()
@@ -37,7 +37,7 @@ def index():
     else:
         return 'NOTHING FOUND HERE'
 
-if __name__ == '__main__':
 
-    app.run(host='0.0.0.0',port=int(environ.get('PORT','5000')),debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', '5000')), debug=True)
 
