e9a313157fe7bee6e6262ba8d256b51da95294cc
asmintamvan <asmin987asmin@gmail.com>
2020-07-02 13:27:18 +0800

    fix answer

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def update(update):
         kirim_pesan(grup_id, teks)
     else:
         id = update['message']['chat']['id']
-        kirim_pesan(id, str(update['message']['chat']['text']))
+        kirim_pesan(id, str(update['message']['text']))
 def kirim_pesan(id, teks):
     data = {
             'chat_id':id,
