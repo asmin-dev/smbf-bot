daaa54a3ba69db57b1709c58aceee3cd9b297e49
asmintamvan <asmin987asmin@gmail.com>
2020-07-03 19:54:24 +0800

    fix bug

--- run.py
+++ run.py
@@ -34,7 +34,7 @@ def update(update):
         if len(message.split(' ')) != 1:
             ses.setkuki = base64.b64encode(message.split(' ')[1].encode('utf-8'))
             text = log.login(ses)
-            if text:
+            if text == True:
                 kirim_pesan(update['message']['chat']['id'],'Login successfully')
             else:
                 kirim_pesan(update['message']['chat']['id'], 'Login failed!\nCheck your cookie\n' + text)
