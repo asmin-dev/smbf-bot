ac2417bab5e1de6c25c9c3a11b3dee75197b9dc8
asmintamvan <asmin987asmin@gmail.com>
2020-07-03 21:27:27 +0800

    fix bug

--- session/__init__.py
+++ session/__init__.py
@@ -43,8 +43,8 @@ class Account:
     def loged(self):
         self.__login = False
     def login(self,ses):
-        return str(ses.get('/me').content)
-        #if 'mbasic_logout_button' in str(ses.get("/me").content):
-         #   return True
+        #return str(ses.get('/me').content)
+        if 'mbasic_logout_button' in str(ses.get("/me").content):
+            return 'success'
         #else:
          #   return str(ses.get("/me").content)
