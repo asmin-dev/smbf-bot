52d34694cf7b6430196fb28d6b64271e70a7b715
asmintamvan <asmin987asmin@gmail.com>
2020-07-05 08:53:26 +0800

    fix bug

--- run.py
+++ run.py
@@ -4,7 +4,6 @@
 from flask import Flask, request
 from session.session import Browser
 from os import environ
-import base64
 import session
 import requests
 
@@ -15,37 +14,44 @@ url = 'https://api.telegram.org/bot' + environ["TOKEN"]
 
 
 
+def data(data):
+    try:
+        text = str(update['message']['text'])
+        id = update['message']['chat']['id']
+        return {'text':text, 'id':id}
+    except:
+        return None
 
 def update(update):
-    try:
-        message = str(update['message']['text'])
-    except:
-        message = ''
+    data = data(update)
     if 'new_chat_member' in str(update):
         nama_grup = update['message']['chat']['title']
         grup_id   = update['message']['chat']['id']
         mem_baru  = update['message']['new_chat_member']['first_name']
         teks      = f'Hai {mem_baru} !\nSelamat datang di Grup {nama_grup}'
-        kirim_pesan(grup_id, teks)
-    elif message.lower().startswith('/about'):
+        send(grup_id, teks)
+    elif data['text'].lower().startswith('/about'):
         text = 'Hy, i\'m Smbf bot\nI was made for find random account on facebook\nBut I am still in the development stage\nI was made by t.me/asmindev'
-        kirim_pesan(update['message']['chat']['id'], text)
-    elif message.startswith('/login'):
-        if len(message.split(' ')) != 1:
-            ses.setkuki = message.split(' ',1)[1].replace(' ','')
-            text = log.login(ses)
-            if text:
-               kirim_pesan(update['message']['chat']['id'], 'Login successfully')
-               kirim_pesan(update['message']['chat']['id'], str(log.__str__(ses)))
+        send(data['id'], text)
+    elif data['text'].startswith('/login'):
+        if len(data['text'].split(' ')) != 1:
+            ses.setkuki = data['text'].split(' ',1)[1].replace(' ','')
+            if log.login(ses):
+               send(data['id'], 'Login successfully')
+               #kirim_pesan(update['message']['chat']['id'], str(log.__str__(ses)))
             else:
-                kirim_pesan(update['message']['chat']['id'], 'Login failed!\nCheck your cookie')
+                send(data['id'], 'Login failed!\nCheck your cookie')
         else:
-            kirim_pesan(update['message']['chat']['id'], 'Usage:\n\t/login <your cookie here>')
+            send(data['id'], 'Usage:\n\t/login <your cookie here>')
+    elif data['text'].startswith('/myinfo'):
+        if not log.logged:
+            send(data['id'], 'You must login')
+        else:
+            send(data['id'], log.__str__(ses))
     else:
-        id = update['message']['chat']['id']
-        kirim_pesan(id, str(update['message']['text']))
-def kirim_pesan(id, teks):
-    data = {'chat_id':id,'text':teks}
+        send(data['id'], data['text'])
+def send(id, teks):
+    data = {'chat_id':id,'text':str(teks)}
     requests.get(url  + '/sendMessage', params=data)
 
 @app.route('/',methods=['POST', 'GET'])
